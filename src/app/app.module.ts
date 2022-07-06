dd87d8cf5b40950245cf1a571a9997b382704018
so33na33 <santhoshsona150@gmail.com>
2022-07-06 12:27:30 +0530

    faculty_name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentNameComponent } from './department-name/department-name.component';
+import { FacultyNameComponent } from './faculty-name/faculty-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentNameComponent
+    DepartmentNameComponent,
+    FacultyNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/faculty-name/faculty-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-name',
+  templateUrl: './faculty-name.component.html',
+  styleUrls: ['./faculty-name.component.css']
+})
+export class FacultyNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
